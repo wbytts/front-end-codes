3574c5270c85c875d1e75400635616660ccd1265
wangbingyi <1041493283@qq.com>
2022-05-01 17:10:01 +0800

    update

---
+++ jest/test/sum.test.js
@@ -0,0 +1,34 @@
+﻿const sum = require('../src/sum.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('test-hello', () => {
+  expect(1+1).toBe(2);
+  expect(1+2).toBe(3);
+  expect(2+2).toBe(4);
+});
+
+/*
+exspect(运行结果).toBe(期望的结果);
+//常见断言方法
+expect({a:1}).toBe({a:1})//判断两个对象是否相等
+expect(1).not.toBe(2)//判断不等
+expect({ a: 1, foo: { b: 2 } }).toEqual({ a: 1, foo: { b: 2 } })
+expect(n).toBeNull(); //判断是否为null
+expect(n).toBeUndefined(); //判断是否为undefined
+expect(n).toBeDefined(); //判断结果与toBeUndefined相反
+expect(n).toBeTruthy(); //判断结果为true
+expect(n).toBeFalsy(); //判断结果为false
+expect(value).toBeGreaterThan(3); //大于3
+expect(value).toBeGreaterThanOrEqual(3.5); //大于等于3.5
+expect(value).toBeLessThan(5); //小于5
+expect(value).toBeLessThanOrEqual(4.5); //小于等于4.5
+expect(value).toBeCloseTo(0.3); // 浮点数判断相等
+expect('Christoph').toMatch(/stop/); //正则表达式判断
+expect(['one','two']).toContain('one'); //不解释
+*/
+
+
+
